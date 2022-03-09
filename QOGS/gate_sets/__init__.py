504e419fed569f50287abcaaf73ac2d1c7008f81
Jacob Curtis <jacobcurtis24@outlook.com>
2022-03-09 09:32:37 -0500

    add ECD two mode to __init__

--- QOGS/gate_sets/__init__.py
+++ QOGS/gate_sets/__init__.py
@@ -1,3 +1,4 @@
 from QOGS.gate_sets.gate_set import GateSet
 from QOGS.gate_sets.ECD_gate_set import ECDGateSet
 from QOGS.gate_sets.SNAP_disp import SNAP_disp
+from QOGS.gate_sets.two_mode_ECD import TwoModeECDGateSet
