1d0b4a8065e94fbdd9c38cd416d4aa9a4c403eb1
Jacob Curtis <jacobcurtis24@outlook.com>
2022-06-10 00:52:12 +0000

    work on conditional fidelity

--- QOGS/gate_sets/PI_GRAPE.py
+++ QOGS/gate_sets/PI_GRAPE.py
@@ -50,22 +50,25 @@ class PI_GRAPE(GRAPE):
             forwards_arr = forwards_arr.write(k, inits)
         forwards = forwards_arr.stack() # [time index, batch/multistart index, initial state index, vector index]
 
-        # now calculate backward propogated states
+        # now apply jump operators to those states
+        one_jump_states = tf.einsum("ij,kmsj...->kmsi...", self.jump_ops, forwards) # each forward state after a jump, may need to treat final time step carefully if jump_op != 
+        one_jump_state_norms = tf.einsum("kmsi...,kmsi...->kms...", tf.math.conj(one_jump_states), one_jump_states) # calculate state norms
+        one_jump_states = tf.einsum("kmsi...,kms...->kmsi...", one_jump_states, 1 / one_jump_state_norms) # renormalize after jump
 
-        backwards_arr = tf.TensorArray(tf.complex64, size=bs.shape[0], dynamic_size=False) # [time index, batch/multistart index, initial state index, vector index]
-        backwards_arr = backwards_arr.write(0, finals)
+        # now we need to finish propogating the forward states after the jumps
 
-        for k in tf.range(bs.shape[0]):
-            finals = tf.einsum(
-                "mij,msj->msi", tf.linalg.adjoint(bs[bs.shape[0] - 1 - k, ...]), finals # run the blocks backwards, so we propagate backwards in time from the final state
+        one_jump_state_arr = tf.TensorArray(tf.complex64, size=bs.shape[0], dynamic_size=False, clear_after_read=False) # [time index, batch/multistart index, initial state index, vector index]
+        one_jump_state_arr = one_jump_state_arr.write(bs.shape[0] - 1, one_jump_states[-1, ...])
+
+        for k in tf.range(1, bs.shape[0]): # we have already propogated one time step
+            one_jump_states = tf.einsum(
+                "mij,kmsj->kmsi", bs[k, ...], one_jump_states # propogates all states, but we only save the finished one per loop. over-propogated states are ignored
             )  # m: multistart, s:multiple states
-            backwards_arr = backwards_arr.write(bs.shape[0] - 1 - k, finals) # order so that the first entry of backwards is the fully back-propagated final state, else we calculate overlaps of states with equal numbers of forward/back prop
-        backwards = backwards_arr.stack() # [time index, batch/multistart index, initial state index, vector index]
-
-        one_jump_states = tf.einsum("ij,kmsj...->kmsi...", self.jump_ops, forwards) # each forward state after a jump
-        one_jump_state_norms = tf.einsum("kmsi...,kmsi...->kms...", tf.math.conj(one_jump_states), one_jump_states) # renormalize after jump
-        one_jump_states = tf.einsum("kmsi...,kms...->kmsi...", one_jump_states, 1 / one_jump_state_norms)
-        one_jump_overlaps = self.jump_weights * tf.reduce_mean(tf.einsum("kmsi...,kmsi...->kms...", tf.math.conj(backwards), one_jump_states), axis=[2]) # calculate overlaps with single jumps inserted
+            one_jump_state_arr = one_jump_state_arr.write(bs.shape[0] - k - 1, one_jump_states[bs.shape[0] - k - 1, ...]) # save the finished state
+        one_jumps = one_jump_state_arr.stack()
+        
+        raise(NotImplemented) # continue and calculate conditional fidelity
+        one_jump_overlaps = self.jump_weights * tf.reduce_mean(tf.einsum("kmsi...,kmsi...->kms...", self.target_states_conj, one_jumps), axis=[2]) # calculate overlaps with single jumps inserted
         no_jump_overlaps = (1 - self.jump_weights) * tf.reduce_mean(tf.einsum("si...,msi...->ms...", self.target_states_conj, forwards[-1, :, :, :]), axis=1) # averaging over initial/final states
         
         one_jump_overlaps = tf.squeeze(one_jump_overlaps)
